e0eb45ca9c7c4d7a8847abca2d1214a20e19627f
Trish Whetzel <plwhetzel@gmail.com>
2016-07-02 23:45:19 -0700

    first pass at gettig data from View and checking for correct answer

--- QuizApp/app/src/main/java/com/trishwhetzel/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/trishwhetzel/quizapp/MainActivity.java
@@ -1,15 +1,22 @@
 package com.trishwhetzel.quizapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +29,57 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // Get results of quiz responses
+                String myMessage = getQuizResponses();
+
+                // Display results with score
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context, myMessage+score, duration);
+                toast.show();
+
+                // Reset score
+                score = 0;
+
+//                Snackbar.make(view, myMessage+" Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
+    }
+
+    /**
+     * Get quiz responses
+     */
+    public String getQuizResponses() {
+        // Get Answer 1
+        EditText q1_answer = (EditText) findViewById(R.id.beer_wagon);
+        String q1_answer_text = q1_answer.getText().toString();
+        q1_answer_text = q1_answer_text.trim();
+        Log.d(TAG, "Q1 Answer:" + q1_answer_text);
+        int q1_score = checkAnswerQ1(q1_answer_text);
+        Log.d(TAG, "Q1-Score:"+q1_score);
+
+        // Get Answer 2
+        CheckBox q2_answer = (CheckBox) findViewById(R.id.checkbox4);
+        String q2_answer_text = q2_answer.getText().toString();
+        Log.d(TAG, "Num hooves:"+q2_answer_text);
+
+        // Get Answer 3
+
+
+        String scoreMessage = "You answered the Quiz!";
+        return scoreMessage;
+    }
+
+    private int checkAnswerQ1(String q1_answer_text) {
+        Log.d(TAG, "User-Answer:"+q1_answer_text);
+        // Check if text is answer Clydesdale
+        String correctAnswer = getString(R.string.q1_correct_answer);
+        if (q1_answer_text.equals(correctAnswer)) {
+            score++;
+            Log.d(TAG, "Correct Q1 Answer");
+        }
+        return score;
     }
 
     @Override
